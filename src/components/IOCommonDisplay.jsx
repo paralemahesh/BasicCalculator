e798109582c7cae1d956b1e2d050b4ba2d9b2f46
Mahesh Parale <maheshparale10@gmail.com>
2019-08-07 00:00:49 +0530

    Trying out Radium

--- src/components/IOCommonDisplay.jsx
+++ src/components/IOCommonDisplay.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import css from '../css/modules/ioCommonDisplay.module.css';
+import Radium from 'radium';
 
-export default class IOCommonDisplay extends React.Component{
+class IOCommonDisplay extends React.Component{
     render(){
         return (
             <div className="ioCommonDisplay">
-                <div className={css.ioCommonDisplayInput}>{this.props.input}</div>
-                <div className={css.ioCommonDisplayOutput}>{this.props.output}</div>
+                <div style={[css.commonInput, css.ioCommonDisplayInput]}>{this.props.input}</div>
+                <div style={[css.commonInput, css.ioCommonDisplayOutput]}>{this.props.output}</div>
             </div>
         );
     };
 }
+
+let css = {
+    commonInput:{
+        lineHeight: '50px',
+        margin: '0px 15px',
+        textAlign: 'right',
+    },
+    ioCommonDisplayInput: {
+        fontSize: '25px'
+    },
+    ioCommonDisplayOutput: {
+        fontSize: '30px',
+        color: '#333333'
+    }
+};
+
+export default Radium(IOCommonDisplay);
