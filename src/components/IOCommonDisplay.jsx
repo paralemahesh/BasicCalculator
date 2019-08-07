74ec1f5929d540190f5f3b7c927778d358fdfbee
Mahesh Parale <maheshparale10@gmail.com>
2019-08-07 21:57:38 +0530

    Integration of aphrodite

--- src/components/IOCommonDisplay.jsx
+++ src/components/IOCommonDisplay.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import css from '../css/modules/ioCommonDisplay.module.css';
+import {StyleSheet, css} from 'aphrodite/no-important';
+
+const styles = StyleSheet.create({
+    commonDisplayComponent : {
+        lineHeight: '50px',
+        margin: '0px 15px',
+        textAlign: 'right',
+    },
+    
+    ioCommonDisplayInput : {
+        fontSize: '25px'
+    },
+    
+    ioCommonDisplayOutput: {
+        fontSize: '30px',
+        color: '#333333'
+    }
+});
 
 export default class IOCommonDisplay extends React.Component{
     render(){
         return (
             <div className="ioCommonDisplay">
-                <div className={css.ioCommonDisplayInput}>{this.props.input}</div>
-                <div className={css.ioCommonDisplayOutput}>{this.props.output}</div>
+                <div className={css(styles.commonDisplayComponent, styles.ioCommonDisplayInput)}>{this.props.input}</div>
+                <div className={css(styles.commonDisplayComponent, styles.ioCommonDisplayOutput)}>{this.props.output}</div>
             </div>
         );
     };
